42bfbde73722226ac45c2dd9d6fc032a2c2cd55e
Júlia <juliagasull@icloud.com>
2020-07-22 15:34:59 +0200

    hackernews

---
+++ server.js
@@ -0,0 +1,15 @@
+//Install express server
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/HackerNews'));
+
+app.get('/*', function(req,res) {
+  res.sendFile('index.html', {root: 'dist/HackerNews/'});
+});
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
